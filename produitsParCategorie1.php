17989b8c7ef301ad0a403ea3081dbb13b39b25ea
log13 <youssef.idbenidder@gmail.com>
2021-02-13 08:28:42 +0100

    feature: add register and login

--- produitsParCategorie1.php
+++ produitsParCategorie1.php
@@ -6,7 +6,14 @@
   </head>
 
   <body>
-    <center> <h1> SUPERVENTES </h1> <br/><br/>
+    <center> <h1> SUPERVENTES </h1> 
+	<?php 
+	session_start();
+	if($_SESSION["loggedin"] == true){
+		echo "<h3>utilisateur connecté ".$_SESSION["username"]."</h3>"; 
+	}
+	?>
+	<br/><br/>
 	<div style="float: left; margin-right: 20px; width:33%">
 		<h3> Recherches </h3>
 	  Sélectionnez les produits par une catégorie <br/>
@@ -31,9 +38,9 @@
 		<h3> Résultat de la recherche </h3>
 	<?php
       $dsn = "mysql:host=localhost;dbname=superventes";  /* Data Source Name */
-      $username = "root"; $password = "";
+      $db_username = "root"; $db_password = "";
       $options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');
-      $dbh = new PDO($dsn, $username, $password, $options) or die("Pb de connexion !");
+      $dbh = new PDO($dsn, $db_username, $db_password, $options) or die("Pb de connexion !");
 
 	  $where = "";
 	  if (isset($_GET['catégorie'])) {
@@ -57,7 +64,148 @@
       echo "</table>";
 	?>
 	</div>
+
+	<div style="float:left;  width:33%">
+        <h2>Authentification</h2>
+        <form action="http://localhost/produitsParCategorie1.php" method="post">
+            <div >
+                <label>Email</label>
+                <input type="email" name="login_username" value="<?php echo $login_username; ?>">
+                <span ><?php echo $login_username_err; ?></span>
+            </div>    
+            <div>
+                <label>Password</label>
+                <input type="password" name="login_password">
+                <span ><?php echo $login_password_err; ?></span>
+            </div>
+            <div>
+                <input type="submit" value="Se connecter">
+            </div>
+        </form>
+    </div>
+
+	<?php
 	
+	if($_SERVER["REQUEST_METHOD"] == "POST"){
+		
+		// Validate username and password
+		if(empty(trim($_POST["login_username"])) && empty(trim($_POST["login_password"]))){
+			$login_username_err = "Please enter a username.";
+		} else{
+			echo "<h1> here </h1>";
+			$login_username = $_POST["login_username"];
+			$login_password = $_POST["login_password"];
+			// Prepare a select statement
+			$where = " WHERE email='$login_username' and password='$login_password'";
+			$sql = "SELECT * FROM clients $where;";
+			$sth = $dbh->prepare($sql);
+      		/* Les données que le SGBD nous renvoie sont stockées en mémoire */
+			try{
+				$sth->execute(); 
+			}catch(PDOException $e) {
+				echo $sql . "<br>" . $e->getMessage();
+			  }
+      		$result_login = $sth->fetchAll(); /* Les données sont recopiées dans le tableau result */
+			  
+			  if (empty($result_login)){
+				
+				$login_username_err = "User not found";
+				} else{
+					session_start();        
+                    // Store data in session variables
+                    $_SESSION["loggedin"] = true;
+                    $_SESSION["username"] = $login_username;
+				}
+			  }
+		}
+	?>
+
+	<div style="float:left;  width:33%">
+        <h2>Inscription</h2>
+        <form action="http://localhost/produitsParCategorie1.php" method="post">
+            <div class="form-group <?php echo (!empty($register_username_err)) ? 'has-error' : ''; ?>">
+                <label>Email</label>
+                <input  type="email" name="register_username" class="form-control" value="<?php echo $register_username; ?>">
+                <span ><?php echo $register_username_err; ?></span>
+            </div>    
+            <div class="form-group <?php echo (!empty($register_password_err)) ? 'has-error' : ''; ?>">
+                <label>Password</label>
+                <input type="password" name="register_password" class="form-control">
+                <span class="help-block"><?php echo $register_password_err; ?></span>
+            </div>
+			<div class="form-group <?php echo (!empty($nom_err)) ? 'has-error' : ''; ?>">
+                <label>nom</label>
+                <input  name="nom" class="form-control" value="<?php echo $nom; ?>">
+                <span ><?php echo $nom_err; ?></span>
+            </div>    
+            <div class="form-group <?php echo (!empty($prenom_err)) ? 'has-error' : ''; ?>">
+                <label>prenom</label>
+                <input name="prenom" class="form-control">
+                <span class="help-block"><?php echo $prenom_err; ?></span>
+            </div>
+            <div >
+                <input type="submit" value="S'inscrire">
+            </div>
+        </form>
+    </div>  
+	
+	<?php
+	// Processing form data when form is submitted
+	if($_SERVER["REQUEST_METHOD"] == "POST"){
+		
+		// Validate username
+		if(empty(trim($_POST["register_username"]))){
+			$username_err = "Please enter a username.";
+		} else{
+			$register_username = $_POST["register_username"];
+			// Prepare a select statement
+			$where = " WHERE email='$register_username'";
+			$sql = "SELECT * FROM clients $where;";
+			$sth = $dbh->prepare($sql);
+      		/* Les données que le SGBD nous renvoie sont stockées en mémoire */
+			try{
+				$sth->execute(); 
+			}catch(PDOException $e) {
+				echo $sql . "<br>" . $e->getMessage();
+			  }
+      		$result_login = $sth->fetchAll(); /* Les données sont recopiées dans le tableau result */
+			  
+			  if (! empty($result_login)){
+				
+				$register_username_err = "This username is already taken.";
+				} else{
+				$register_username = trim($_POST["register_username"]);
+				
+				}
+			  }
+		}
+	
+		// Validate password
+		if(empty(trim($_POST["register_password"]))){
+			$register_password_err = "Please enter a password.";     
+		} elseif(strlen(trim($_POST["register_password"])) < 6){
+			$register_password_err = "Password must have atleast 6 characters.";
+		} else{
+			$register_password = trim($_POST["register_password"]);
+		}
+		
+		$nom = $_POST['nom'];
+		$prenom = $_POST['prenom'];
+		
+		// Check input errors before inserting in database
+		if(empty($register_username_err) && empty($register_password_err) ){
+			
+			// Prepare an insert statement
+			$sql = "INSERT INTO clients (email, password, nomClient, prénom) VALUES ('$register_username', '$register_password', '$nom', '$prenom')";
+			 try{
+				$sth = $dbh->prepare($sql);
+				$sth->execute();
+			 }catch(PDOException $e) {
+				echo $sql . "<br>" . $e->getMessage();
+			  }       		
+		}
+	?>
+
 	<?php
 	  if (isset($_GET['numProduit'])) {
 		$sql = "INSERT INTO produitsPaniers (emailClient, numProduit, quantité) VALUES
